c97aea4b84c1ca42dc2f6e3d8053a4679f630e5c
Adrian Flores <jesus2407087@hybridge.education>
2025-11-02 19:42:15 -0600

    Corregir ejercicio01.js - función obtenerNombre con export correcto

--- ejercicio01.js
+++ ejercicio01.js
@@ -2,3 +2,9 @@ function obtenerNombre() {
     const nombre = "Adrián Flores";
     return nombre;
 }
+
+module.exports = { obtenerNombre };function obtenerNombre() {
+    const nombre = "Adrián Flores";
+    return nombre;
+module.exports = { obtenerNombre };
+}
